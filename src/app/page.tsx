44200848319609416813577990879cfe3fcac3f5
dayal <dayal.gaitonde@gmail.com>
2024-09-02 14:49:14 -0700

    push test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold mb-8">Todo App</h1>
+      <h1 className="text-4xl font-bold mb-8">Todo AppXX</h1>
       <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} />
     </main>
